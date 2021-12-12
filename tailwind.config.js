8d4edac375b22f0a08fa6d9a2fe9a6f7111487c4
linus <linusang@outlook.com>
2021-12-12 21:30:57 +0800

    updated to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
