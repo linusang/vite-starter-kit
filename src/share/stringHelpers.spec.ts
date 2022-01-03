8b538d9deb2132327b84d6cfc23b71490cb75531
linus <linusang@outlook.com>
2022-01-03 23:30:47 +0800

    clean up

--- src/share/stringHelpers.spec.ts
+++ src/share/stringHelpers.spec.ts
@@ -1,23 +1,21 @@
 import { isNullOrEmpty } from "./stringHelpers";
-import { expect, describe, it } from "@jest/globals";
+import { expect, describe, test } from "@jest/globals";
 
-describe("stringHelpers", () => {
-  describe("isNullOrEmpty", () => {
-    it("should return true if null", () => {
-      const result = isNullOrEmpty(null);
-      expect(result).toBeTruthy();
-    });
-    it("should return true if undefined", () => {
-      const result = isNullOrEmpty(undefined);
-      expect(result).toBeTruthy();
-    });
-    it("should return true if empty", () => {
-      const result = isNullOrEmpty("");
-      expect(result).toBeTruthy();
-    });
-    it("should return false if has string", () => {
-      const result = isNullOrEmpty("hello world");
-      expect(result).toBeFalsy();
-    });
+describe("testing isNullOrEmpty()", () => {
+  test("should return true if null", () => {
+    const result = isNullOrEmpty(null);
+    expect(result).toBeTruthy();
+  });
+  test("should return true if undefined", () => {
+    const result = isNullOrEmpty(undefined);
+    expect(result).toBeTruthy();
+  });
+  test("should return true if empty", () => {
+    const result = isNullOrEmpty("");
+    expect(result).toBeTruthy();
+  });
+  test("should return false if has string", () => {
+    const result = isNullOrEmpty("hello world");
+    expect(result).toBeFalsy();
   });
 });
