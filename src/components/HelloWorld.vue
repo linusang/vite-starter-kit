8cf7b165e98a934e075493e93119c71d4289e80c
Linus Ang <linusang@outlook.com>
2022-02-06 19:41:32 +0800

    added pretter-plugin-tailwindcss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
+  <div class="flex h-screen items-center justify-center">
     <div class="text-center">
       <img alt="Vue logo" src="@/assets/logo.png" class="mx-auto" />
       <div class="space-y-5">
@@ -22,14 +22,14 @@
         </p>
         <button
           type="button"
-          class="border p-3 rounded shadow-md bg-gray-300 border-gray-600"
+          class="rounded border border-gray-600 bg-gray-300 p-3 shadow-md"
           @click="countClicked"
         >
           count is: {{ count }}
         </button>
         <p>
           Edit
-          <code class="bg-gray-100 rounded text-[#304455] px-2 py-1"
+          <code class="rounded bg-gray-100 px-2 py-1 text-[#304455]"
             >components/HelloWorld.vue</code
           >
           to test hot module replacement.
