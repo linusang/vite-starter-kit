2e61d4dd8888a53465b1ac823ed217d26a023823
Linus Ang <linusang@yahoo.com>
2023-04-24 22:05:52 +0800

    remove reactivity transform

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      reactivityTransform: true,
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
