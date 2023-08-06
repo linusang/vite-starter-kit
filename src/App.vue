1064e857569392c4efbe529f3b31407476445460
Linus Ang <linusang@yahoo.com>
2023-08-06 21:52:21 +0800

    reordered vue tags

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
 <template>
   <HelloWorld
     msg="Hello Vue 3 + TypeScript + Vite + Tailwind + Vitest + Cypress + ESLint + Prettier"
   />
 </template>
-
-<script setup lang="ts">
-  import HelloWorld from "./components/HelloWorld.vue";
-</script>
