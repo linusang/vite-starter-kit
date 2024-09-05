4a73167513123651d739940e86d10d202a5009b0
Linus Ang <linusang@yahoo.com>
2024-09-05 21:42:14 +0800

    packages dep update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import Vue from "@vitejs/plugin-vue";
 import * as path from "path";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vitest/config";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), Icons({ compiler: "vue3" })],
+  plugins: [Vue(), Icons({ compiler: "vue3" }), vueDevTools()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
