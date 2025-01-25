e6a0aad1972025a6337560d7933eb6713809edec
Linus Ang <linusang@yahoo.com>
2025-01-25 23:46:53 +0800

    use tailwind vite plugin instead

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import icons from "unplugin-icons/vite";
 import { fileURLToPath } from "url";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { defineConfig } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), icons({ compiler: "vue3" }), vueDevTools()],
+  plugins: [tailwindcss(), vue(), icons({ compiler: "vue3" }), vueDevTools()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
